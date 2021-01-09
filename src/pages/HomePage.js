e54f7cac5cbb2b5cb05d9dd4fa5eae2e93ede7c2
mcheung008 <mcheung008@gmail.com>
2021-01-09 14:22:30 -0800

    built out jumbotron

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Hero from '../components/Hero';
+
 
 function HomePage(props) {
 
     return(
-        <p>HomePage works!</p>
+        <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
     );
 
 }
