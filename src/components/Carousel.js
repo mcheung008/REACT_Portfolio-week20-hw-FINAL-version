c02b516d5ac7ceeb934f1383fd098a3ee706d383
mcheung008 <mcheung008@gmail.com>
2021-01-09 22:41:58 -0800

    added cards

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Card from '../components/Card';
+
 import sportsPage from '../assets/images/sportsPage.png';
 import filmConnector from '../assets/images/filmConnector.png';
+
 
 
 class Carousel extends React.Component {
@@ -27,9 +32,38 @@ class Carousel extends React.Component {
         }
     }
 
+    handleCardClick = (id, card) => {
+        console.log(id);
+        let items = [...this.state.items];
+
+        items[id].selected = items[id].selected ? false : true;
+
+        items.forEach(item => {
+            if (item.id !== id) {
+                item.selected = false;
+            }
+        });
+
+        this.setState({
+            items
+        });
+
+    }
+
+
+    makeItems = (items) => {
+        return items.map(item=> {
+            return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
+        })
+    }
+
     render() {
         return(
-            <p>Carousel works!</p>
+            <Container fluid={true}>
+                <Row className="justify-content-around">
+                    {this.makeItems(this.state.items)}
+                </Row>
+            </Container>
         );
     }
 }
